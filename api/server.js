ac4e0e985360db9b6ed689b8cdf9f3dcb53cfa5b
Anthony Cedeño <thonyjbc@gmail.com>
2021-11-04 13:26:49 -0400

    Now the API can serve the app.
    Added dependency fastify-static to serve the build files in the app.
    The API listen the get method in the "/" route.

--- api/server.js
+++ api/server.js
@@ -2,11 +2,15 @@ const fastify = require("fastify")({ logger: true })
 const productsRoutes = require("./app/routes/products.routes")
 const imagesRoutes = require("./app/routes/images.routes")
 const { port, host } = require("./app/config/env")
+const { join } = require("path")
 
 fastify.register(require("fastify-cors"), { origin: "*" })
-fastify.register(require("fastify-file-upload"), {
-  createParentPath: true,
+fastify.register(require("fastify-file-upload"), { createParentPath: true })
+fastify.register(require("fastify-static"), {
+  root: join(__dirname, "../app/build"),
 })
+
+fastify.get("/", (req, reply) => reply.sendFile("index.html"))
 
 productsRoutes.forEach(route => fastify.route(route))
 imagesRoutes.forEach(route => fastify.route(route))
