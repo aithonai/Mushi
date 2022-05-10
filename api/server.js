816fb91cf0a537cad5cde42b2ef92dc9bf55c0c4
Anthony-jcb <thonyjbc@gmail.com>
2022-05-10 00:18:46 -0400

    Litle changes in server and exported logger

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
-const log = require("pino")({ prettyPrint: true })
-const fastify = require("fastify")({ logger: log })
-const productsRoutes = require("./app/routes/products.routes")
-const imagesRoutes = require("./app/routes/images.routes")
+const log = require("./app/server/logger")
+const fastify = require("fastify")({ log })
+const routes = require("./app/routes")
 const { port, host } = require("./app/config/env")
 const { join } = require("path")
 
@@ -11,16 +10,12 @@ fastify.register(require("fastify-static"), { root: join(__dirname, "../app/buil
 
 fastify.get('*', (req, reply) => reply.sendFile("index.html"))
 
-productsRoutes.forEach(route => fastify.route(route))
-imagesRoutes.forEach(route => fastify.route(route))
+routes.products.forEach(route => fastify.route(route))
+routes.images.forEach(route => fastify.route(route))
 
-const start = async () => {
-  try {
-    await fastify.listen({ host, port })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-
-start()
+fastify.listen({ host, port })
+.then(adress => log.info(adress))
+.catch(err => {
+  log.error(err)
+  process.exit(1)
+})
