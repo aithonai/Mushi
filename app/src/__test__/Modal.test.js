a244e1719f113f750ad86c9871ab60a118d76cc6
Anthony-jcb <thonyjbc@gmail.com>
2022-05-10 21:33:25 -0400

    Added new test in Modal

--- app/src/__test__/Modal.test.js
+++ app/src/__test__/Modal.test.js
@@ -4,11 +4,18 @@ import { render, screen, fireEvent } from "@testing-library/react"
 import Modal from "../components/Modal"
 import { useModal } from "../hooks/useModal"
 import { NewCard, Title, Thumbnail } from "../components/CardStyles"
-import { ModalContainer } from "../components/ModalStyles"
+import { ModalContainer, CloseButton } from "../components/ModalStyles"
 
-describe("Modal works", () => {
-	test("Modal open", () => {
+describe("Close modal properly", () => {
+	test("Modal close using close button", () => {
+		const { container } = render(<Modal />)
+		const modalContainer = container.querySelector(ModalContainer)
+		const closeButton = container.querySelector(CloseButton)
 
+		fireEvent.click(modalContainer)
+		fireEvent.click(closeButton)
+
+		expect(modalContainer).toHaveStyleRule('display', 'none')
 	})
 
 	test("Modal close when click darken zone", () => {
