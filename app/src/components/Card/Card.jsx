a315b9f0173f4e44e752444e3fe55cd51cd4e31b
aithonai <aithonaidev@gmail.com>
2022-06-14 00:21:43 -0400

    Refactorized Card, now import Modal and render CardModal in it.

--- app/src/components/Card/Card.jsx
+++ app/src/components/Card/Card.jsx
@@ -1,21 +1,40 @@
-import { NewCard, Thumbnail, Information, Title, Description } from "./CardStyles"
+import {
+  Container,
+  Thumbnail,
+  Information,
+  Title,
+  Description,
+  Price,
+} from "./CardStyles"
 import { useModal } from "../../hooks/useModal"
+import Modal from "../Modal"
 import CardModal from "./CardModal"
 
-export default function Card(props) {
-  const {name, description, thumbnail} = props
-  const [modalCard, openModalCard, closeModalCard] = useModal()
-
+export default function CardContainer(props) {
+  const { name, description, thumbnail, price } = props
+  const [active, open, close] = useModal()
+  
   return (
-    <NewCard onClick={openModalCard}> 
-      <Thumbnail>
-        {thumbnail && thumbnail.length > 0 && <img src={thumbnail[0]} alt={name + "thumbnail"} />}
-      </Thumbnail> 
-      <Information>
-        <Title>{name}</Title>
-        <Description>{description}</Description>
-      </Information>
-      <CardModal {...props} isOpen={modalCard} closeModal={closeModalCard} />
-    </NewCard>
+    <>
+      <Container onClick={open}>
+        <Thumbnail>
+          {thumbnail && thumbnail.length > 0 && (
+            <img src={thumbnail[0]} alt={name + "thumbnail"} />
+          )}
+        </Thumbnail>
+        <Information>
+          <div>
+            <Title>{name}</Title>
+            <Description>{description}</Description>
+          </div>
+          <div>
+            <Price>{price && "$" + price}</Price>
+          </div>
+        </Information>
+      </Container>
+      <Modal {...props} active={active} close={close}>
+        <CardModal {...props} close={close}/>
+      </Modal>
+    </>
   )
 }
