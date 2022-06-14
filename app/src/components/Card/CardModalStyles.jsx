bca2137a33d80fef826d4efb5bf2c56300688dc4
aithonai <aithonaidev@gmail.com>
2022-06-14 00:26:51 -0400

    Removed unnecessary margin

--- app/src/components/Card/CardModalStyles.jsx
+++ app/src/components/Card/CardModalStyles.jsx
@@ -40,9 +40,8 @@ export const Navigation = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  width: calc(100% - 2rem);
+  width: 100%;
   height: 1.5rem;
-  margin: 0 1rem;
   padding: 1rem 0;
 `
 
